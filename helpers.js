c1452bf32842b5744a74945b782ab9a0eebb823d
Mate Gombkoto <mategombhu@gmail.com>
2021-10-20 12:33:14 +0200

    Initial commit

---
+++ helpers.js
@@ -0,0 +1,49 @@
+// helper functions to make dom manipulation a bit less tedious
+// import them into your js files with the following:
+// import { $, html, insertFromTemplate, insertText } from "./helpers.js";
+
+function $(selector) {
+	const selection = document.querySelectorAll(selector);
+	if (selection.length === 1) {
+		return selection[0];
+	} else if (selection.length > 1) {
+		return selection;
+	}
+}
+
+// for html support in template literals with the lit-html visual studio code extension
+function html(markup) {
+	return markup;
+}
+
+function insertFromTemplate(templateSelector, locationSelector) {
+	const templateSelection = $(templateSelector);
+
+	function cloneTemplate(template, location) {
+		const clone = template.content.cloneNode(true);
+		location.appendChild(clone);
+	}
+
+	const locationSelection = $(locationSelector);
+
+	if (locationSelection.length === 1) {
+		cloneTemplate(templateSelection, locationSelection);
+	} else if (locationSelection.length > 1) {
+		for (const location of locationSelection) {
+			cloneTemplate(templateSelection, location);
+		}
+	}
+}
+
+function insertText(text, selector) {
+	const selection = $(selector);
+	if (selection.length === 1) {
+		selection.textContent = text;
+	} else if (selection.length > 1) {
+		for (const element of selection) {
+			element.textContent = text;
+		}
+	}
+}
+
+export { $, html, insertFromTemplate, insertText };
